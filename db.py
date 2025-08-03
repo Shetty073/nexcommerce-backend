939ca11982f6238c65ecd93955edfee2c7aba5cc
Ashish Shetty <shetty073@gmail.com>
2025-08-04 00:45:25 +0530

    correct typing

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 # db.py
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from typing import AsyncGenerator
 from config import settings
 
 # Create async engine (note the async database URL)
@@ -8,6 +9,6 @@ engine = create_async_engine(settings.database_url, echo=settings.debug)
 # Create async session maker
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-async def get_async_session() -> AsyncSession:
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
